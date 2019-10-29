72a8f547b59fd534528fcd752e95da44c1c3e2be
Ans de Nijs <ansdenijs@gmail.com>
2019-10-29 09:01:22 +0100

    Update toxicity.js

--- static/toxicity.js
+++ static/toxicity.js
@@ -1,15 +1,24 @@
 toxicity.load().then(model => {
-  $('#loader').removeClass('active')
+  $("#loader").removeClass("active");
 
-  $('#comment-form').submit(function () {
-    const text = $('textarea', this).val()
+  $("#comment-form").submit(function() {
+    const text = $("textarea", this).val();
 
     // The minimum prediction confidence.
-    const threshold = 0.5;
+    const threshold = 0.8;
+    const labelsToInclude = [
+      "threat",
+      "obscene",
+      "identity_attack",
+      "insult",
+      "severe_toxicity",
+      "sexual_explicit",
+      "toxicity"
+    ];
 
     // Load the model. Users optionally pass in a threshold and an array of
     // labels to include.
-    toxicity.load(threshold).then(model => {
+    toxicity.load(threshold, labelsToInclude).then(model => {
       model.classify([text]).then(predictions => {
         // `predictions` is an array of objects, one for each prediction head,
         // that contains the raw probabilities for each input along with the
@@ -19,6 +28,6 @@ toxicity.load().then(model => {
       });
     });
 
-    return false
-  })
-})
+    return false;
+  });
+});
